08a70fc73b7f2f61f988701c6ca51136204057b0
Dan Belsey <dan.belsey@babylonhealth.com>
2019-08-08 13:59:04 +0100

    convert actions/goals to function components, add state

--- src/module/Action.js
+++ src/module/Action.js
@@ -2,43 +2,36 @@ import * as React from "react";
 
 import './module.css';
 
-class Action extends React.Component {
+function Action(props) {
 
-    constructor(props) {
-        super(props);
-        this.state = props.action;
-        this.handleActionNameChange = this.handleActionNameChange.bind(this);
-        this.handleTargetTypeChange = this.handleTargetTypeChange.bind(this);
+    const {action, onUpdate} = props;
+
+    const onNameChange = (event) => {
+        onUpdate({...action, name: event.target.value});
     }
 
-    handleActionNameChange(event) {
-        this.setState({name: event.target.value});
+    const onTypeChange = (event) => {
+        onUpdate({...action, targetType: event.target.value});
     }
 
-    handleTargetTypeChange(event) {
-        this.setState({targetType: event.target.value});
-    }
-
-    render() {
-        return (
-            <div className='well'>
-                <div className="form-group">
-                    <label> Action Name:
-                        <input name="actionName" className="form-control" type="text" value={this.state.name}
-                               onChange={this.handleActionNameChange}
-                        ></input>
-                    </label>
-                </div>
-                <div className="form-group">
-                    <label> Target Type:
-                        <input name="targetType" className="form-control" type="text" value={this.state.targetType}
-                               onChange={this.handleTargetTypeChange}
-                        ></input>
-                    </label>
-                </div>
+    return (
+        <div className='well'>
+            <div className="form-group">
+                <label> Action Name:
+                    <input name="actionName" className="form-control" type="text" value={action.name}
+                           onChange={onNameChange}
+                    ></input>
+                </label>
             </div>
-        );
-    }
+            <div className="form-group">
+                <label> Target Type:
+                    <input name="targetType" className="form-control" type="text" value={action.targetType}
+                           onChange={onTypeChange}
+                    ></input>
+                </label>
+            </div>
+        </div>
+    );
 
 }
 
--- src/module/Module.js
+++ src/module/Module.js
@@ -9,6 +9,15 @@ function Module(props) {
     const [goals, setGoals] = React.useState(props.module.goals);
     const [actions, setActions] = React.useState(props.module.actions);
 
+    const updateGoal = (updatedGoal, index) => {
+        //updating arrays _must_ update the reference; probably better ways than this but :shrug: cba installing lodash
+        setGoals(goals.map((g, i) => (i === index) ? updatedGoal : g));
+    }
+
+    const updateAction = (updatedAction, index) => {
+        setActions(actions.map((a, i) => (i === index) ? updatedAction : a));
+    }
+
     return (
         <form>
             <h1>Module Maker</h1>
@@ -16,21 +25,21 @@ function Module(props) {
             <div className="well form-group">
                 <label> Module Name:
                     <input name="moduleName" className="form-control" type="text" value={name}
-                           onChange={setName}
+                           onChange={(e) => setName(e.target.value)}
                     ></input>
                 </label>
             </div>
             <div className='wellRow'>
                 <div className="well orange">
                     <h4>Goals</h4>
-                    {goals.map(goal =>
-                        <Goal goal={goal} className='wellItem'/>
+                    {goals.map((goal, index) =>
+                        <Goal goal={goal} onUpdate={(updated) => updateGoal(updated, index)} className='wellItem'/>
                     )}
                 </div>
                 <div className="well yellow">
                     <h4>Actions</h4>
-                    {actions.map(action =>
-                        <Action action={action} className='wellItem'/>
+                    {actions.map((action, index) =>
+                        <Action action={action} onUpdate={(updated) => updateAction(updated, index)} className='wellItem'/>
                     )}
                 </div>
             </div>
