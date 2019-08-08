60cc4f6470d0dfe8cc3968001491943a002ecef4
Dan Belsey <dan.belsey@babylonhealth.com>
2019-08-08 13:35:40 +0100

    Convert Module to function component, begin adding state hooks

--- src/module/Module.js
+++ src/module/Module.js
@@ -4,49 +4,38 @@ import Goal from "./Goal";
 
 import './module.css';
 
-class Module extends React.Component {
+function Module(props) {
+    const [name, setName] = React.useState(props.module.name);
+    const [goals, setGoals] = React.useState(props.module.goals);
+    const [actions, setActions] = React.useState(props.module.actions);
 
-    constructor(props) {
-        super(props);
-        this.state = props.module;
-        this.handleChange = this.handleChange.bind(this);
-    }
+    return (
+        <form>
+            <h1>Module Maker</h1>
 
-    handleChange(event) {
-        this.setState({name: event.target.value});
-    }
-
-    render() {
-        return (
-            <form>
-
-                <marquee>Module maker!!</marquee>
-
-                <div className="well form-group">
-                    <label> Module Name:
-                        <input name="moduleName" className="form-control" type="text" value={this.state.name}
-                               onChange={this.handleChange}
-                        ></input>
-                    </label>
+            <div className="well form-group">
+                <label> Module Name:
+                    <input name="moduleName" className="form-control" type="text" value={name}
+                           onChange={setName}
+                    ></input>
+                </label>
+            </div>
+            <div className='wellRow'>
+                <div className="well orange">
+                    <h4>Goals</h4>
+                    {goals.map(goal =>
+                        <Goal goal={goal} className='wellItem'/>
+                    )}
                 </div>
-                <div className='wellRow'>
-                    <div className="well yellow">
-                        <h4>Actions</h4>
-                        {this.state.actions.map(action =>
-                            <Action action={action} className='wellItem'/>
-                        )}
-                    </div>
-                    <div className="well orange">
-                        <h4>Goals</h4>
-                        {this.state.goals.map(goal =>
-                            <Goal goal={goal} className='wellItem'/>
-                        )}
-                    </div>
+                <div className="well yellow">
+                    <h4>Actions</h4>
+                    {actions.map(action =>
+                        <Action action={action} className='wellItem'/>
+                    )}
                 </div>
-            </form>
-        );
-    }
-
+            </div>
+        </form>
+    );
 }
 
 export default Module;
